af69b9e3014163d1ae8bcf568aa4c83fa0873184
thatkhay <johnkingsleyegeonu@gmail.com>
2024-07-27 04:36:08 +0100

    fix

--- src/app/components/modals/DeleteUser.tsx
+++ src/app/components/modals/DeleteUser.tsx
@@ -1,59 +1,54 @@
 import React from 'react';
 
 type Props = {
-	handleDelete: () => void;
+  handleDelete: () => void;
+  deleteUser: () => void;
 };
 
-const DeleteUser: React.FC<Props> = ({ handleDelete }) => {
-	return (
-		<>
-			<div
-				className='relative z-10'
-				aria-labelledby='modal-title'
-				role='dialog'
-				aria-modal='true'>
-				<div
-					className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'
-					aria-hidden='true'></div>
-				<div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-					<div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-						<div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-							<div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
-								<div className='sm:flex sm:items-start'>
-									<div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
-										<h3
-											className='leading-6 text-gray-900 text-center text-2xl mb-8'
-											id='modal-title'>
-											Delete this user
-										</h3>
-										<div className='mt-2'>
-											<p className='text-sm text-[#667185]'>
-												This user and all associated data will be permanently
-												removed. Do you wish to continue
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className=' px-4 py-3 flex justify-center gap-x-6 mb-6'>
-								<button
-									type='button'
-									className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
-									onClick={() => handleDelete()}>
-									Cancel action
-								</button>
-								<button
-									type='button'
-									className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'>
-									Delete
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+const DeleteUser: React.FC<Props> = ({ handleDelete, deleteUser }) => {
+  return (
+    <>
+      <div className='relative z-10' aria-labelledby='modal-title' role='dialog' aria-modal='true'>
+        <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' aria-hidden='true'></div>
+        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+            <div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+              <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+                <div className='sm:flex sm:items-start'>
+                  <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+                    <h3 className='leading-6 text-gray-900 text-center text-2xl mb-8' id='modal-title'>
+                      Delete this user
+                    </h3>
+                    <div className='mt-2'>
+                      <p className='text-sm text-[#667185]'>
+                        This user and all associated data will be permanently removed. Do you wish to continue?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='px-4 py-3 flex justify-center gap-x-6 mb-6'>
+                <button
+                  type='button'
+                  className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+                  onClick={() => handleDelete()}
+                >
+                  Cancel action
+                </button>
+                <button
+                  type='button'
+                  className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+                  onClick={() => deleteUser()}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DeleteUser;
--- src/app/components/table/Table.tsx
+++ src/app/components/table/Table.tsx
@@ -8,177 +8,201 @@ import UpdateUser from "../modals/UpdateUser";
 import DeleteUser from "../modals/DeleteUser";
 import { toast } from "react-toastify";
 import Image from "next/image";
-import filter from '../../../assets/filter.svg';
-import add from '../../../assets/addUser.svg';
-import navigate from '../../../assets/navigation.svg';
+import filterIcon from "../../../assets/filter.svg";
 
 type User = {
-	id: number;
-	email: string;
-	name: string;
-	role: string;
+  id: number;
+  email: string;
+  name: string;
+  role: string;
 };
 
 const Table: React.FC = () => {
-	const [create, setCreate] = useState(false);
-	const [update, setUpdate] = useState(false);
-	const [deleted, setDeleted] = useState(false);
-	const [loading, setLoading] = useState(true);
-	const [users, setUsers] = useState<User[]>([]);
-	const [error, setError] = useState<string | null>(null);
+  const [create, setCreate] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-	const handleCreate = () => setCreate(!create);
-	const handleUpdate = () => setUpdate(!update);
-	const handleDelete = () => setDeleted(!deleted);
+  const handleCreate = () => setCreate(!create);
+  const handleUpdate = () => setUpdate(!update);
+  const handleDelete = () => setDeleted(!deleted);
 
-	// Fetch users from API
-	const getUsers = async () => {
-		setLoading(true);
-		try {
-			const response = await fetch('https://voyatek-api.free.beeceptor.com/api/get-user');
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			const data = await response.json();
-			setUsers(data.data);
-		} catch (error) {
-			setError('Failed to fetch users'); // Set error message
-			console.error('Error fetching users:', error);
-		} finally {
-			setLoading(false);
-		}
-	};
+  // Fetch users from API
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://voyatek-api.free.beeceptor.com/api/get-user"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setUsers(data.data);
+    } catch (error) {
+      setError("Failed to fetch users"); // Set error message
+      console.error("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		getUsers();
-	}, []);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-	const addUser = (newUser: User) => {
-		setUsers([...users, newUser]);
-		toast.success("User created successfully!");
-	};
+  const addUser = (newUser: User) => {
+    setUsers([...users, newUser]);
+    toast.success("User created successfully!");
+  };
 
-	return (
-		<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-			<table className="w-full text-sm text-left rtl:text-right">
-				<thead className="text-xs text-[#334155] font-[400] bg-[#F0F2F5]">
-					<tr className="bg-white">
-						<td className="px-6 py-4" colSpan={2}>
+  const deleteUser = async (userId: number) => {
+    try {
+      // Assume there is an API endpoint to delete the user
+      const response = await fetch(
+        `https://voyatek-api.free.beeceptor.com/api/delete-user/${userId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      // Update state to remove the user
+      setUsers(users.filter((user) => user.id !== userId));
+      toast.success("User deleted successfully!");
+      handleDelete();
+    } catch (error) {
+      setError("Failed to delete user"); // Set error message
+      console.error("Error deleting user:", error);
+      toast.error("Failed to delete user");
+    }
+  };
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right">
+        <thead className="text-xs text-[#334155] font-[400] bg-[#F0F2F5]">
+          <tr className="bg-white">
+            <td className="px-6 py-4" colSpan={2}>
               <div className="flex items-center gap-x-4">
                 <TableInput />
                 <button
                   type="submit"
-                  className="flex items-center gap-x-2 focus:ring-blue-300 font-medium border rounded-lg text-xs px-4 py-2"
+                  className="focus:ring-blue-300 font-medium border rounded-lg text-xs px-4 py-2 flex gap-x-2 items-center"
                 >
-                  <Image
-                    src={filter} 
-                    width={15}
-                    height={15}
-                    alt="icon"
-                  />
+                  <Image src={filterIcon} width={15} height={15} alt="icon" />
                   Filter
                 </button>
               </div>
             </td>
-					<td className="px-6 py-4" colSpan={2}>
-						<div className="flex justify-end">
-							
-							<button
-								type="submit"
-								onClick={() => setCreate(true)}
-								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-4 py-2 flex gap-x-2 items-center"
-							>
-								<Image src={add} width={15} height={15} alt='icon' />{' '}
-								New User
-							</button>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<th scope="col" className="px-6 py-3 font-[500] bg-[#F0F2F5]">
-						<div className="flex gap-x-2 items-center">
-							<TableCheckBox />
-							Name <span><Image src={navigate} width={10} height={10} alt='icon' className="mx-2" />{' '}</span>
-						</div>
-					</th>
-					<th scope="col" className="px-6 py-3 font-[500]">
-						<p className="flex gap-x-2">Email Address <span><Image src={navigate} width={10} height={10} alt='icon' className="mx-2" />{' '}</span></p> 
-					</th>
-					<th scope="col" className="px-6 py-3 font-[500] flex">
-						<p className="flex gap-x-2">Role <span><Image src={navigate} width={10} height={10} alt='icon' className="mx-2" />{' '}</span></p> 
-						
-					</th>
-					<th scope="col" className="px-6 py-3 font-[500]">
-						<p className="flex gap-x-2">Actions <span><Image src={navigate} width={10} height={10} alt='icon' className="mx-2" />{' '}</span></p>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				{loading ? (
-					<tr>
-						<td className="text-center" colSpan={4}>
-							Loading Users...
-						</td>
-					</tr>
-				) : error ? (
-					<tr>
-						<td className="text-center text-red-500" colSpan={4}>
-							{error}
-						</td>
-					</tr>
-				) : users.length > 0 ? (
-					users.map((user: User) => (
-						<tr className="bg-white border-[#F0F2F5] border-b-4" key={user.id}>
-							<th scope="row" className="px-6 py-4 whitespace-nowrap font-normal">
-								<div className="flex gap-x-2 items-center">
-									<TableCheckBox />
-									{user.name}
-								</div>
-							</th>
-							<td className="px-6 py-4">{user.email}</td>
-							<td className="px-6 py-4">
-								<p
-									className={`py-1 px-2 rounded-[25px] text-center w-max ${user.role === "Administrator"
-										? "text-[#0D6EFD] bg-[#F0F6FE]"
-										: user.role === "Sales Manager"
-											? "text-[#10B981] bg-[#D1FAE5]"
-											: "text-[#F58A07] bg-[#FEF4E6]"
-
-										}`}
-								>
-									{user.role}
-								</p>
-							</td>
-							<td className="px-6 py-4 text-xs">
-								<span
-									className="text-[#0D6EFD] mr-4 cursor-pointer"
-									onClick={() => handleUpdate()}
-								>
-									View
-								</span>
-								<span
-									className="text-[#98A2B3] cursor-pointer"
-									onClick={() => handleDelete()}
-								>
-									Remove
-								</span>
-							</td>
-						</tr>
-					))
-				) : (
-					<tr>
-						<td className="text-center text-red-500" colSpan={4}>
-							No User Found
-						</td>
-					</tr>
-				)}
-			</tbody>
-		</table>
-			{ create && <CreateUser handleCreate={handleCreate} addUser={addUser} /> }
-	{ update && <UpdateUser handleUpdate={handleUpdate} /> }
-	{ deleted && <DeleteUser handleDelete={handleDelete} /> }
-		</div >
-	);
+            <td className="px-6 py-4" colSpan={2}>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  onClick={() => setCreate(true)}
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-4 py-2 flex gap-x-2 items-center"
+                >
+                  New User
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th scope="col" className="px-6 py-3 font-[500] bg-[#F0F2F5]">
+              <div className="flex gap-x-2 items-center">
+                <TableCheckBox />
+                Name
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3 font-[500]">
+              <p className="flex gap-x-2">Email Address</p>
+            </th>
+            <th scope="col" className="px-6 py-3 font-[500]">
+              <p className="flex gap-x-2">Role</p>
+            </th>
+            <th scope="col" className="px-6 py-3 font-[500]">
+              <p className="flex gap-x-2">Actions</p>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr>
+              <td className="text-center" colSpan={4}>
+                Loading Users...
+              </td>
+            </tr>
+          ) : error ? (
+            <tr>
+              <td className="text-center text-red-500" colSpan={4}>
+                {error}
+              </td>
+            </tr>
+          ) : users.length > 0 ? (
+            users.map((user: User) => (
+              <tr className="bg-white border-[#F0F2F5] border-b-4" key={user.id}>
+                <th
+                  scope="row"
+                  className="px-6 py-4 whitespace-nowrap font-normal"
+                >
+                  <div className="flex gap-x-2 items-center">
+                    <TableCheckBox />
+                    {user.name}
+                  </div>
+                </th>
+                <td className="px-6 py-4">{user.email}</td>
+                <td className="px-6 py-4">
+                  <p
+                    className={`py-1 px-2 rounded-[25px] text-center w-max ${
+                      user.role === "Administrator"
+                        ? "text-[#0D6EFD] bg-[#F0F6FE]"
+                        : user.role === "Sales Manager"
+                        ? "text-[#10B981] bg-[#D1FAE5]"
+                        : "text-[#F58A07] bg-[#FEF4E6]"
+                    }`}
+                  >
+                    {user.role}
+                  </p>
+                </td>
+                <td className="px-6 py-4 text-xs">
+                  <span
+                    className="text-[#0D6EFD] mr-4 cursor-pointer"
+                    onClick={() => handleUpdate()}
+                  >
+                    View
+                  </span>
+                  <span
+                    className="text-[#98A2B3] cursor-pointer"
+                    onClick={() => {
+                      setSelectedUser(user);
+                      handleDelete();
+                    }}
+                  >
+                    Remove
+                  </span>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td className="text-center text-red-500" colSpan={4}>
+                No User Found
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      {create && <CreateUser handleCreate={handleCreate} addUser={addUser} />}
+      {update && <UpdateUser handleUpdate={handleUpdate} />}
+      {deleted && selectedUser && (
+        <DeleteUser handleDelete={handleDelete} deleteUser={() => deleteUser(selectedUser.id)} />
+      )}
+    </div>
+  );
 };
 
 export default Table;
